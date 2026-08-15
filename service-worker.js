c81c66dbfa5a3b556aad446f7d373882153c068e
uzaylilarsolanakullaniyor <mustafakkavalci@icloud.com>
2026-08-15 12:53:01 +0300

    Move command suggestions upward

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
-const SHELL_CACHE = "numbrrr-shell-v109";
+const SHELL_CACHE = "numbrrr-shell-v110";
 const DATA_CACHE = "numbrrr-data-v2";
 const APP_SHELL = [
   "/",
   "/index.html",
-  "/styles.css?v=92",
+  "/styles.css?v=93",
   "/command-parser.js?v=4",
   "/app.js?v=75",
   "/manifest.webmanifest",

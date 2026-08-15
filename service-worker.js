3f03dc2756cd3023af02c6d7dd52dfa2fa097e14
uzaylilarsolanakullaniyor <mustafakkavalci@icloud.com>
2026-08-15 12:21:32 +0300

    Use countdown icon for countdown tiles

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
-const SHELL_CACHE = "numbrrr-shell-v105";
+const SHELL_CACHE = "numbrrr-shell-v106";
 const DATA_CACHE = "numbrrr-data-v2";
 const APP_SHELL = [
   "/",
   "/index.html",
-  "/styles.css?v=88",
+  "/styles.css?v=89",
   "/command-parser.js?v=4",
   "/app.js?v=75",
   "/manifest.webmanifest",

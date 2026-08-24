a0f17f95cf238bf51b36d7928910e6bfafd6691d
uzaylilarsolanakullaniyor <mustafakkavalci@icloud.com>
2026-08-24 21:14:41 +0300

    Make price bubbles two units wide

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
-const SHELL_CACHE = "numbrrr-shell-v161";
+const SHELL_CACHE = "numbrrr-shell-v162";
 const DATA_CACHE = "numbrrr-data-v2";
 const APP_SHELL = [
   "/",
   "/index.html",
   "/styles.css?v=114",
-  "/fff-theme.css?v=24",
+  "/fff-theme.css?v=25",
   "/command-parser.js?v=4",
   "/app.js?v=95",
   "/manifest.webmanifest",

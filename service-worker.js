24f5f03458d08b7fc7a0c41aa15fb0ad9c9fd8e9
uzaylilarsolanakullaniyor <mustafakkavalci@icloud.com>
2026-08-25 11:35:07 +0300

    Redesign FFF vehicle module

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
-const SHELL_CACHE = "numbrrr-shell-v214";
+const SHELL_CACHE = "numbrrr-shell-v215";
 const DATA_CACHE = "numbrrr-data-v2";
 const APP_SHELL = [
   "/",
   "/index.html",
   "/styles.css?v=114",
-  "/fff-theme.css?v=69",
+  "/fff-theme.css?v=70",
   "/command-parser.js?v=4",
   "/app.js?v=105",
   "/manifest.webmanifest",
